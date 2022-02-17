33732b5f88c6df19199cf306533f99359ebc008f
amirj <amirjahan454@gmail.com>
2022-02-17 19:50:08 +0330

    first project not important

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-        dash amir
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const App=()=>
+{
+  return(
+      <React.Fragment>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda debitis incidunt nihil quo sit soluta tempore voluptatum. Ab atque, aut cumque facere, illum ipsa minus natus nemo non saepe, suscipit.
+      </React.Fragment>
+  )
 }
 
 export default App;
+
+// const App=()=>
+// {
+//     return()
+// }
+//
+// function App()
+// {
+//     return()
+// }
