7c2d84318ac574c327a47e42d966b7c8319ebf13
amirj <amirjahan454@gmail.com>
2022-03-03 18:52:29 +0330

    start project AJstore

---
+++ src/components/navbar/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavbarLG from "./navbarLG/navbarLG";
+import NavbarSM from "./navbarSM/navabrSM";
+
+
+
+
+
+const Navbar = () => {
+
+
+    return(
+        <NavbarLG/>
+        // <NavbarSM/>
+    )
+}
+
+
+export default Navbar
---
+++ src/components/navbar/navbarSM/navabrSM.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+const NavbarSM = () =>
+{
+    return(
+        <nav className="w-full bg-red-700 h-96">
+
+        </nav>
+    )
+}
+
+export default NavbarSM
+
