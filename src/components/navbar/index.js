bae091e223945e8bac2742dff8d42c5832e9f14e
amirj <amirjahan454@gmail.com>
2022-03-10 18:31:03 +0330

    responsive navbar finish && add react router dom

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import NavbarLG from "./navbarLG/navbarLG";
-import NavbarSM from "./navbarSM/navabrSM";
+import React,{useState}  from 'react'
 
+import {Link , BrowserRouter as Router} from "react-router-dom";
+import { Twirl as Hamburger } from 'hamburger-react'
+
+
+import {ReactComponent as Logo} from './navbar-logo.svg'
+
+import 'remixicon/fonts/remixicon.css'
+import "./navbar.scss"
 
 
 
@@ -9,9 +15,63 @@ import NavbarSM from "./navbarSM/navabrSM";
 const Navbar = () => {
 
 
+    const [logoHover, setLogoHover] = useState(false)
+    const [isOpen, setOpen] = useState(false)
+
+
+
     return(
-        <NavbarLG/>
-        // <NavbarSM/>
+        <Router>
+            <div className="lg:hidden md:flex sm:flex flex justify-between items-center">
+                <Hamburger toggled={isOpen} toggle={setOpen} />
+                {
+                    !isOpen ?
+                        <div className="flex justify-center mt-2">
+                            <Link to="/">
+                                <img src="./img/navbar-logo/logo.jpg" alt="AJ_store" className="w-1/2" style={{ width:"100px" , height:"50px" }} />
+                            </Link>
+                        </div>
+                        :
+                        null
+                }
+
+            </div>
+            <nav className={`w-full flex lg:flex-row md:flex-col sm:flex-col flex-col justify-evenly items-center ${isOpen ? "active" : null}`} id="header">
+
+                <div className="lg:w-2/12 md:w-full sm:w-full w-full flex justify-center mt-2">
+                    <Link to="/">
+                        <img src="./img/navbar-logo/logo.jpg" alt="AJ_store" className="w-1/2" style={{ width:"200px" , height:"100px" }} />
+                    </Link>
+                </div>
+
+                <ul className="lg:w-6/12 md:w-full sm:w-full w-full flex lg:flex-row md:flex-col sm:flex-col flex-col lg:justify-evenly md:justify-center sm:justify-center justify-center">
+                    <li className="text-center mt-5"> <Link to="/#"> <i className="ri-home-7-line relative top-0.5"/> Home       </Link> </li>
+                    <li className="text-center mt-5"> <Link to="/#"> <i className="ri-customer-service-line relative top-0.5"/> Services   </Link> </li>
+                    <li className="text-center mt-5"> <Link to="/#"> <i className="ri-shopping-bag-3-line relative top-0.5"/> Products   </Link> </li>
+                    <li className="text-center mt-5"> <Link to="/#"> <i className="ri-information-line relative top-0.5"/> About US   </Link> </li>
+                    <li className="text-center mt-5"> <Link to="/#"> <i className="ri-contacts-line relative top-0.5"/> Contact US </Link> </li>
+                </ul>
+
+                <div className="lg:w-2/12 md:w-full sm:w-full w-full flex justify-center px-10">
+                    <button
+                        onMouseEnter={()=>setLogoHover(true)}
+                        onMouseLeave={()=>setLogoHover(false)}
+                        className="mt-3 bg-gray-800 hover:bg-blue-900 transition duration-300 text-white font-bold py-2 px-4 rounded flex lg:justify-between items-center w-full flex md:justify-center sm:justify-center justify-center">
+                        {
+                            logoHover
+                                ?
+                                <img src="./img/navbar-logo/logo.gif"  alt="AJ_store" style={{ height:'40px'}} />
+                                :
+                                <Logo style={{ height:'40px'}}/>
+                        }
+                        <span className="ml-2">
+                        Your Cart
+                    </span>
+                    </button>
+                </div>
+
+            </nav>
+        </Router>
     )
 }
 
